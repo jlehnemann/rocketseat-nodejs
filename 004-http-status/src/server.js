78a114a4ad3069ddb7ccdc48d7b6eab7c5d11e80
Jose <j.lehnemann@gmail.com>
2026-07-13 21:07:17 -0300

    added .gitignore file

---
+++ 004-http-status/src/server.js
@@ -0,0 +1,34 @@
+import http from 'node:http'
+
+//cabeçalhos (requisição/resposta) => são metadados 
+//metadados => informações para que back e front saibam lidar com req/res da melhor forma
+
+//aplicação aqui será stateful (ela armazena o estado - salva em memória) - oposto de stateless 
+const users = []
+
+const server = http.createServer((req, res) => {
+
+    const { method, url } = req
+
+
+    if(method === 'GET' && url === '/users') {
+
+        return res.setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
+    }
+
+    if (method === 'POST' && url === '/users') {
+
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@example.com'
+        })
+
+        return res.end("Criação de usuários")
+    }
+
+    return res.end("Rota de retorno para rotas diferentes de POST e END")
+} )
+
+server.listen(3333)
